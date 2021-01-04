3be74053686438b947cffd2d8878427db716daa1
adairhu <huminme@outlook.com>
2021-01-04 11:37:23 +0800

    信号与槽实现案例

--- src/designer/RunMainWinMenuToolbar.py
+++ src/designer/RunMainWinMenuToolbar.py
@@ -1,13 +1,38 @@
 import sys
-import MainWinMenuToolbar
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QFileDialog
+from MainWinMenuToolbar import Ui_MainWindow
+from ChildrenForm import Ui_ChildrenForm
 
+class MainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainForm, self).__init__()
+        self.setupUi(self)
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+        # 创建子窗口实例
+        self.child = ChildrenForm()
+
+        # 添加信号与槽函数
+        self.fileCloseAction.triggered.connect(self.close)
+        self.fileOpenAction.triggered.connect(self.openMsg)
+        self.addWinAction.triggered.connect(self.childShow)
+
+    def childShow(self):
+        # 添加子窗口
+        self.MaingridLayout.addWidget(self.child)
+        self.child.show()
+
+    def openMsg(self):
+        file, ok = QFileDialog.getOpenFileName(self, "打开", "c:/", "All Files (*);;Text Files (*.txt)")
+        self.statusBar.showMessage(file, timeout=0)
+
+class ChildrenForm(QWidget, Ui_ChildrenForm):
+    def __init__(self):
+        super(ChildrenForm, self).__init__()
+        self.setupUi(self)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWindow = QMainWin dow()
-    ui = MainWinMenuToolbar.Ui_MainWindow()
-    ui.setupUi(mainWindow)
-    mainWindow.show()
+    win = MainForm()
+    win.show()
     sys.exit(app.exec_())

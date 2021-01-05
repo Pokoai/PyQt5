be43daeae9eec6c29cf64b952606840890389f7d
adairhu <huminme@outlook.com>
2021-01-05 18:32:48 +0800

    增加MatplotlibWidget控件的使用测试

--- src/controls/TextEdit.py
+++ src/controls/TextEdit.py
@@ -1,22 +1,26 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QTextEdit, QVBoxLayout
 from PyQt5.QtGui import QIcon
+# 引入自定义的控件类
+from MatplotlibWidget import MatplotlibWidget
 
 class TextEdit(QWidget):
     # 初始化
     def __init__(self):
         super(TextEdit, self).__init__()
         self.setWindowTitle('QTextEdit 例子')
-        self.resize(300, 270)
+        self.resize(500, 400)
         self.setWindowIcon(QIcon('./images/sandiantu.ico'))
 
         # 创建控件
+        self.matplot = MatplotlibWidget() # 自定义的控件
         self.textEdit = QTextEdit()
         self.button1 = QPushButton('显示文本')
         self.button2 = QPushButton('显示HTML')
 
         # 布局
         layout = QVBoxLayout()
+        layout.addWidget(self.matplot) # 自定义的控件
         layout.addWidget(self.textEdit)
         layout.addWidget(self.button1)
         layout.addWidget(self.button2)
